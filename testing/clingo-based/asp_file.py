8076dbc93d9dbeec048b58e474c898606ca43731
Jindra Mautška <ardnij123@gmail.com>
2024-03-03 16:31:07 +0100

    Remove insignificant code
    Remove code for numpy evaluation in python for asp_file.py
    This program is made for evaluation using clingo

--- testing/clingo-based/asp_file.py
+++ testing/clingo-based/asp_file.py
@@ -24,28 +24,6 @@ def getarray(f: str, dtype=None, shape=None, trans=False) -> np.ndarray:
     return arr
 
 
-# Working with arrays
-
-def layer_lin(
-        inp: array_bin,
-        weight: array_bin) -> array_int:
-    return np.dot(weight, inp)
-
-
-def layer_bn(
-        inp: array_int,
-        bias: array_real) -> array_real:
-    return inp - bias
-
-
-def layer_bin(inp: array_real) -> array_bin:
-    return 1 - 2*np.signbit(inp)
-
-
-def arg_max(inp):
-    return np.argmax(inp)
-
-
 # Classes for using neural network
 
 class Block:
@@ -53,9 +31,6 @@ class Block:
     weight: array_bin
 
     def __init__(self, folder: str):
-        pass
-
-    def comp(self, inp: array_bin):
         pass
 
     def layer(self) -> int:
@@ -110,15 +85,6 @@ class InterBlk(Block):
         self.bias += np.sum(self.weight, axis=1, keepdims=True)
         self.bias /= 2
 
-    def comp(self, inp: array_bin) -> array_bin:
-        # NOT CORRECT FOR PY IMPLEMENTATION
-        return layer_bin(
-                layer_bn(
-                    layer_lin(inp, self.weight),
-                    self.bias
-                    )
-                )
-
 
 class OutputBlk(Block):
     bias: array_real
@@ -129,12 +95,6 @@ class OutputBlk(Block):
         self.weight = getarray(folder+"lin_weight.csv", dtype=np.int8)
         self.bias += np.sum(self.weight, axis=1, keepdims=True)
         self.bias /= 2
-
-    def comp(self, inp: array_bin) -> np.int8:
-        # NOT CORRECT FOR PY IMPLEMENTATION
-        return arg_max(
-                layer_lin(inp, self.weight) + self.bias
-                )
 
     def args(self) -> List[int]:
         bs = (self.bias % 1).flatten()
@@ -152,17 +112,6 @@ class NeuralNetw:
         for i in range(1, inners+1):
             self.network.append(InterBlk(f"{folder}blk{i}/"))
         self.output = OutputBlk(f"{folder}out_blk/")
-
-    def comp(self, inp: array_bin) -> np.int8:
-        for net in self.network:
-            inp = net.comp(inp)
-        return self.output.comp(inp)
-
-    def test(self, generator: Generator[array_bin, None, None]) -> array_int:
-        frequencies = np.zeros(self.output.bias.shape, dtype=np.int64)
-        for inp in generator:
-            frequencies[self.comp(inp)] += 1
-        return frequencies
 
     def layers(self):
         for layer, net in enumerate(self.network + [self.output]):
@@ -203,10 +152,13 @@ if __name__ == "__main__":
     outpre(N, O) :- (N, O) = @args().
     """)
     ctl.ground([("base", [])], context=NeuralNetw(folder))
+    assert type(ctl.configuration.solve) is clingo.Configuration
     ctl.configuration.solve.models = "0"
 
     models = 0
-    with ctl.solve(yield_=True) as handle:
+    solve = ctl.solve(yield_=True)
+    assert type(solve) is clingo.SolveHandle
+    with solve as handle:
         for model in handle:
             models += 1
             if models % 10000 == 0:
