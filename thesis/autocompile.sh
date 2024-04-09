0ad31f6a46446fd037babcef7416d81b252c3272
Jindra Mautška <ardnij123@gmail.com>
2024-04-09 08:01:02 +0200

    Thesis - citations
    added citations and bibliography.bib file
    better tables (using booktables)
    changes in TeX code to be linter-friendly

--- thesis/autocompile.sh
+++ thesis/autocompile.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 # Run this to automatically recompile thesis each time it is updated
 
-file="prace.tex"
+file="prace"
+bib="bibliography"
 
 lastUpdate=0
+lastBibUpdate=0
 while [ true ]; do
     sleep 1
-    nowUpdate="`stat --format='%Y' $file`"
+    nowUpdate="`stat --format='%Y' "$file.tex"`"
     if [ $lastUpdate != $nowUpdate ]; then
+        # recompile 
         lastUpdate=$nowUpdate
-        pdflatex $file 1> /dev/null
-        echo "Updated: $nowUpdate"
-        echo "-----------------------------------------"
+        pdflatex "$file.tex" 1> /dev/null
+        echo "Updated tex: $nowUpdate"
+    fi
+    bibUpdate="`stat --format='%Y' "$bib.bib"`"
+    if [ $lastBibUpdate != $bibUpdate ]; then
+        lastBibUpdate=$bibUpdate
+        biber "$file.bcf"
+        pdflatex "$file.tex" 1> /dev/null
+        pdflatex "$file.tex" 1> /dev/null
+        echo "Updated biber: $bibUpdate"
     fi
 done
