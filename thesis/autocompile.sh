a33b29a165cac96f629dd8006519ed9cf1148dd2
Jindra Mautška <ardnij123@gmail.com>
2024-10-09 15:23:22 +0200

    Thesis - definition of robustness
    Added definition of robustness
    Moved chapters into their own files to improve readability of thesis repository

--- thesis/autocompile.sh
+++ thesis/autocompile.sh
@@ -4,11 +4,18 @@
 file="prace"
 bib="bibliography"
 
+if [ -f $1.tex ]; then
+    echo "Compiling $file.tex automatically based on updates of $1.tex"
+    file_check=$1
+else
+    file_check=$file
+fi
+
 lastUpdate=0
 lastBibUpdate=0
 while [ true ]; do
     sleep 1
-    nowUpdate="`stat --format='%Y' "$file.tex"`"
+    nowUpdate="`stat --format='%Y' "$file_check.tex"`"
     if [ $lastUpdate != $nowUpdate ]; then
         # recompile 
         lastUpdate=$nowUpdate
